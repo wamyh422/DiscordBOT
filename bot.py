8d1c81b082dd22e76cddcbfc9563ee4aaee6c9ef
豈銓 張 <egg2915978@gmail.com>
2019-08-08 00:31:56 +0800

    $grab command impoved

--- bot.py
+++ bot.py
@@ -1,5 +1,10 @@
 import discord
 from discord.ext import commands
+import json
+import random
+
+with open('setting.json','r',encoding='utf-8') as jFile:
+    jdata = json.load(jFile)
 
 bot = commands.Bot(command_prefix='$')
 @bot.event
@@ -12,7 +17,25 @@ async def on_ready():
 
 @bot.command()
 async def grab(ctx):
-    await ctx.send("grab grab grab")
+
+    pic_type = random.choice(['.gif','.jpg','.png'])
+    pic_num = '0'
+    if pic_type == '.gif':
+        pic_num = '1'
+    elif pic_type =='.jpg':
+        pic_num ='2'
+    elif pic_type =='.png':
+        pic_num ='3'
+    number = '1'
+    if pic_num == '1':
+        number = str(random.randrange(1,int(jdata['gif_size'])))
+    elif pic_num =='2':
+        number = str(random.randrange(1,int(jdata['jpg_size'])))
+    elif pic_num =='3':
+        number =str(random.randrange(1,int(jdata['png_size'])))
+    
+    pic = discord.File(jdata['pic_path']+pic_num+" ("+number+")"+pic_type)
+    await ctx.send(file=pic)
 
 @bot.command()
 async def hello (ctx, a):
@@ -25,12 +48,12 @@ async def ping(ctx):
 bot.remove_command('help')
 @bot.command()
 async def help(ctx):
-    embed = discord.Embed(title='Pancake',description="A very good Album.List of commands are: ",color=0xeee657)
+    embed = discord.Embed(title='Pancake',description="A very good bot.List of commands are: ",color=0xeee657)
 
-    embed.add_field(name='$grab', value='print "grab grab grab"', inline=False)
+    embed.add_field(name='$grab', value='grab a picture', inline=False)
     embed.add_field(name='$ping', value='print robot\'s ping',inline=False)
     embed.add_field(name='$hello {message}', value='print {message} Hello!!',inline=False)
     await ctx.send(embed=embed)
 
-TOKEN = "NjA3NTUzNzY0NzQzOTcwODMx.XUgnbw.kFw6_sd_e9Jph-9Cq-EX9HNWUE8"
-bot.run(TOKEN)
+
+bot.run(jdata['TOKEN'])
